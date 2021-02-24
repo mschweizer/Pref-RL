cd085afe7f37daaca4555ffb62dc73b902d4ea47
marvin.schweizer <marvin.schweizer@kit.edu>
2021-02-24 11:32:58 +0100

    Merge tests and remove redundant one

--- tests/test_utility_prediction.py
+++ tests/test_utility_prediction.py
@@ -1,6 +1,4 @@
 import torch
-import torch.nn.functional as F
-import torch.optim as optim
 
 from data_generation.experience import Experience, ExperienceBuffer
 
@@ -18,34 +16,3 @@ def test_predict_utility(env, reward_predictor):
     predicted_utility = reward_predictor.predict_utility()
 
     assert predicted_utility.dtype == torch.float32
-
-
-def test_training_changes_model_parameters(choice_model, preference_data_loader):
-    """
-    Testing whether parameters change uses code from  / is based on
-    https://github.com/suriyadeepan/torchtest/blob/66a2c8b669aa23601f64e208463e9449ffc135da/torchtest/torchtest.py#L106
-    """
-
-    # TODO: Clarify if the following modifications to torch have an effect on other tests
-    torch.manual_seed(42)
-    torch.set_deterministic(d=True)
-
-    params = [param for param in choice_model.named_parameters() if param[1].requires_grad]
-    initial_params = [(name, param.clone()) for (name, param) in params]
-
-    data_iter = iter(preference_data_loader)
-    learning_rate = 100000  # large lr to ensure parameters change sufficiently much for the test to succeed
-    optimizer = optim.Adam(choice_model.parameters(), lr=learning_rate)
-    criterion = F.binary_cross_entropy
-
-    optimizer.zero_grad()
-    queries, choices = next(data_iter)
-
-    choice_predictions = choice_model(queries)
-    loss = criterion(choice_predictions, choices)
-
-    loss.backward()
-    optimizer.step()
-
-    for (_, p0), (name, p1) in zip(initial_params, params):
-        assert not torch.equal(p0, p1)
