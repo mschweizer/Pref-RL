c577ebed1ee10a6ce42747dc50558a0f094a01bb
marvin.schweizer <marvin.schweizer@kit.edu>
2021-04-26 15:16:58 +0200

    Add new main for testing

--- teach.py
+++ teach.py
@@ -1,24 +1,7 @@
 import argparse
 
-from agent.rl_agent import RLAgent
+from agent.preference_based.sequential.sequential_pbrl_agent import SequentialPbRLAgent
 from wrappers.utils import create_env
-
-
-def main():
-    parser = create_cli()
-    args = parser.parse_args()
-
-    env = create_env(args.env_id)
-
-    agent = RLAgent(env)
-
-    agent.learn_reward_model(num_pretraining_data=args.num_pretrain_data, pretraining_epochs=args.pretrain_epochs)
-
-    env.close()
-
-
-if __name__ == '__main__':
-    main()
 
 
 def create_cli():
@@ -29,3 +12,20 @@ def create_cli():
     parser.add_argument('--pretrain_epochs', default=5, type=int)
     parser.add_argument('--num_timesteps', default=5e6, type=int)
     return parser
+
+
+def main():
+    parser = create_cli()
+    args = parser.parse_args()
+
+    env = create_env(args.env_id, termination_penalty=10.)
+
+    agent = SequentialPbRLAgent(env=env)
+
+    agent.learn_reward_model(num_training_timesteps=100000, num_pretraining_preferences=500)
+
+    env.close()
+
+
+if __name__ == '__main__':
+    main()
