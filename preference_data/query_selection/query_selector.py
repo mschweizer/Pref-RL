d07e79093901b029ba279413721a1040e5c13d9d
marvin.schweizer <marvin.schweizer@kit.edu>
2021-04-26 15:16:17 +0200

    Add index query selector

--- preference_data/query_selection/query_selector.py
+++ preference_data/query_selection/query_selector.py
@@ -4,17 +4,22 @@ from abc import ABC, abstractmethod
 
 class AbstractQuerySelector(ABC):
 
-    def select_queries(self, queries, num_queries=1):
-        return [self.select_query(queries) for _ in range(num_queries)]
-
-    @staticmethod
     @abstractmethod
-    def select_query(queries):
+    def select_queries(self, queries, num_queries=1):
         pass
 
 
 class RandomQuerySelector(AbstractQuerySelector):
 
+    def select_queries(self, queries, num_queries=1):
+        return [self.select_query(queries) for _ in range(num_queries)]
+
     @staticmethod
     def select_query(queries):
         return random.choice(queries)
+
+
+class IndexQuerySelector(AbstractQuerySelector):
+
+    def select_queries(self, queries, num_queries=1):
+        return queries[:num_queries]
