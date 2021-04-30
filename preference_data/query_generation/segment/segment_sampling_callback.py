9a71b4528088bb25f788e5e15293631d152b242a
marvin.schweizer <marvin.schweizer@kit.edu>
2021-04-30 15:05:05 +0200

    Fix bug so that enough samples are generated

--- preference_data/query_generation/segment/segment_sampling_callback.py
+++ preference_data/query_generation/segment/segment_sampling_callback.py
@@ -1,6 +1,6 @@
 from stable_baselines3.common.callbacks import BaseCallback
 
-from preference_data.query_generation.segment.utils import is_sampling_step, generation_volume_is_reached
+from preference_data.query_generation.segment.utils import is_sampling_step
 
 
 class SegmentSamplingCallback(BaseCallback):
@@ -10,12 +10,14 @@ class SegmentSamplingCallback(BaseCallback):
         self.segment_sampler = segment_sampler
         self.sampling_interval = sampling_interval
         self.generation_volume = generation_volume
+        self.generated_samples_counter = 0
 
     def _on_step(self):
         if self._is_sampling_step():
             sample = self.segment_sampler.try_to_sample()
             if sample:
                 self.segment_sampler.segment_samples.append(sample)
+                self.generated_samples_counter += 1
             if self._generation_volume_is_reached():
                 return False
         else:
@@ -25,5 +27,4 @@ class SegmentSamplingCallback(BaseCallback):
         return is_sampling_step(self.num_timesteps, self.sampling_interval)
 
     def _generation_volume_is_reached(self):
-        return generation_volume_is_reached(self.generation_volume,
-                                            self.segment_sampler.segment_samples)
+        return self.generation_volume and self.generated_samples_counter >= self.generation_volume
