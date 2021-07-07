6b6d5147fe56a52fd8dd0a42edc11670d6bfc14b
marvin.schweizer <marvin.schweizer@kit.edu>
2021-07-07 09:56:19 +0200

    Fix test

--- tests/preference_data/querent/test_oracle.py
+++ tests/preference_data/querent/test_oracle.py
@@ -4,9 +4,9 @@ from preference_data.querent.oracle import RewardMaximizingOracle
 
 
 def test_reward_maximizing_oracle_prefers_higher_reward():
-    segment_1 = [Experience(observation=1, action=1, reward=1, done=1, info={"original_reward": 0}),
-                 Experience(observation=1, action=1, reward=1, done=1, info={"original_reward": 0})]
-    segment_2 = [Experience(observation=1, action=1, reward=1, done=1, info={"original_reward": 25}),
-                 Experience(observation=1, action=1, reward=1, done=1, info={"original_reward": 25})]
+    segment_1 = [Experience(observation=1, action=1, reward=1, done=1, info={"external_reward": 0}),
+                 Experience(observation=1, action=1, reward=1, done=1, info={"external_reward": 0})]
+    segment_2 = [Experience(observation=1, action=1, reward=1, done=1, info={"external_reward": 25}),
+                 Experience(observation=1, action=1, reward=1, done=1, info={"external_reward": 25})]
 
     assert RewardMaximizingOracle().answer(query=[segment_1, segment_2]) == Label.RIGHT
