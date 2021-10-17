71794a08a2bbb1d44a7358c380dfd8e88f8e5e5a
Sascha Nebel <sascha.nebel@student.kit.edu>
2021-10-18 00:19:23 +0200

    Fixed colours of clips

--- video/segment_renderer.py
+++ video/segment_renderer.py
@@ -1,27 +1,24 @@
-from abc import ABC, abstractmethod
-import cv2
 import random
 import numpy as np
-from cv2 import VideoWriter, VideoWriter_fourcc, randn
-from pathlib import Path
+import cv2
+from cv2 import VideoWriter, VideoWriter_fourcc
 
 
 class SegmentRenderer:
 
-    def __init__(self, out):
-        self.out = out
+    def __init__(self, output_path):
+        self.output_path = output_path
 
-    def render_segment(self, query):
-        outfile = self.out + str(random.randint(0, 100000000)) + '.avi'
+    def render_segment(self, segment):
+        outfile = self.output_path + str(random.randint(0, 100000000)) + '.avi'
         fps = 12
         fourcc = VideoWriter_fourcc(*'DIVX')
-        singleframe = np.array(query.frames[0])
+        singleframe = np.array(segment.frames[0])
         fshape = (singleframe.shape[1], singleframe.shape[0])
-        vid_writer = VideoWriter(outfile, fourcc, fps, fshape)
-        #TODO: reverse rgb values in frames
-        for frame in query.frames:
-            vid_writer.write(frame)
 
-        vid_writer.release()    
+        vid_writer = VideoWriter(outfile, fourcc, fps, fshape)
         
-        self.nr = self.nr + 1
+        for frame in segment.frames:
+            vid_writer.write(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+
+        vid_writer.release()
