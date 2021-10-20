e9bfc412f9d25d2ccfaf28bdf8d892d218bb7db6
marvin.schweizer <marvin.schweizer@kit.edu>
2021-10-20 18:24:45 +0200

    Add reward maximizing oracle

--- preference_collection/preference_oracle.py
+++ preference_collection/preference_oracle.py
@@ -32,3 +32,31 @@ class RewardMaximizingOracleMixin(AbstractOracleMixin):
 class RandomOracleMixin(AbstractOracleMixin):
     def answer(self, query):
         return Label.random()
+
+
+class AbstractOracle(ABC):
+
+    @abstractmethod
+    def answer(self, query):
+        pass
+
+
+class RewardMaximizingOracle(AbstractOracle):
+    def answer(self, query):
+        assert len(query.choice_set) == 2, \
+            "Preference oracle assumes choice sets of size 2, but found {num} items.".format(num=len(query.choice_set))
+        reward_1, reward_2 = self.compute_total_original_rewards(query)
+        return self.compute_preference(reward_1, reward_2)
+
+    @staticmethod
+    def compute_total_original_rewards(query):
+        return (sum(info["external_reward"] for info in segment.infos) for segment in query.choice_set)
+
+    @staticmethod
+    def compute_preference(reward_1, reward_2):
+        if reward_1 > reward_2:
+            return Label.LEFT
+        elif reward_1 < reward_2:
+            return Label.RIGHT
+        else:
+            return Label.INDIFFERENT
--- tests/preference_collection/test_oracle.py
+++ tests/preference_collection/test_oracle.py
@@ -1,5 +1,8 @@
+import pytest
+
 from preference_collection.label import Label
-from preference_collection.preference_oracle import RewardMaximizingOracleMixin
+from preference_collection.preference_oracle import RewardMaximizingOracleMixin, RewardMaximizingOracle
+from query_generation.query import ChoiceSetQuery
 from wrappers.internal.trajectory_buffer import Segment
 
 
@@ -10,3 +13,23 @@ def test_reward_maximizing_oracle_prefers_higher_reward():
                         infos=[{"external_reward": 25}, {"external_reward": 25}])
 
     assert RewardMaximizingOracleMixin().answer(query=[segment_1, segment_2]) == Label.RIGHT
+
+
+def test_raises_assertion_error_when_query_set_size_is_not_2():
+    query = ChoiceSetQuery(choice_set=["item1", "item2", "item3"])
+    oracle = RewardMaximizingOracle()
+
+    with pytest.raises(AssertionError):
+        oracle.answer(query)
+
+
+def test_prefers_higher_reward():
+    segment_1 = Segment(observations=[1, 1], actions=[1, 1], rewards=[1, 1], dones=[1, 1],
+                        infos=[{"external_reward": 0}, {"external_reward": 0}])
+    segment_2 = Segment(observations=[1, 1], actions=[1, 1], rewards=[1, 1], dones=[1, 1],
+                        infos=[{"external_reward": 25}, {"external_reward": 25}])
+    query = ChoiceSetQuery(choice_set=[segment_1, segment_2])
+
+    oracle = RewardMaximizingOracle()
+
+    assert oracle.answer(query) == Label.RIGHT
