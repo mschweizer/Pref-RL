599a9a806dc0eeba106717fb601f5734d998bdfb
marvin.schweizer <marvin.schweizer@kit.edu>
2021-10-22 14:48:49 +0200

    Collect synthetic preferences inside querent

--- preference_collection/preference_querent.py
+++ preference_collection/preference_querent.py
@@ -13,8 +13,12 @@ class AbstractPreferenceQuerent(ABC):
 
 class DummyPreferenceQuerent(AbstractPreferenceQuerent):
 
-    def __init__(self, query_selector):
+    def __init__(self, query_selector, preference_collector):
         super(DummyPreferenceQuerent, self).__init__(query_selector)
+        self.preference_collector = preference_collector
 
     def query_preferences(self, query_candidates, num_queries) -> List:
-        return self.query_selector.select_queries(query_candidates, num_queries)
+        newly_pending_queries = self.query_selector.select_queries(query_candidates, num_queries)
+        self.preference_collector.pending_queries.extend(newly_pending_queries)
+        self.preference_collector.collect_preferences()
+        return []
--- tests/preference_collection/test_dummy_preference_querent.py
+++ tests/preference_collection/test_dummy_preference_querent.py
@@ -1,12 +1,17 @@
+from unittest.mock import Mock
+
 from preference_collection.preference_querent import DummyPreferenceQuerent
 from query_selection.query_selector import RandomQuerySelector
 
 
 def test_queries_correct_number_of_queries():
-    preference_querent = DummyPreferenceQuerent(query_selector=RandomQuerySelector())
+    preference_collector = Mock()
+    preference_collector.collect_preferences = Mock()
+    preference_querent = DummyPreferenceQuerent(query_selector=RandomQuerySelector(),
+                                                preference_collector=preference_collector)
     query_candidates = ["query1", "query2"]
     num_preference_queries = 1
 
-    newly_pending_queries = preference_querent.query_preferences(query_candidates, num_queries=num_preference_queries)
+    preference_querent.query_preferences(query_candidates, num_queries=num_preference_queries)
 
-    assert len(newly_pending_queries) == num_preference_queries
+    assert len(preference_collector.pending_queries.extend.call_args.args[0]) == num_preference_queries
