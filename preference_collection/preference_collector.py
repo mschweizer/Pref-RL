1383ec11d5379f79bf2505307ca3f61c14c05068
Sascha Nebel <sascha.nebel@student.kit.edu>
2021-10-25 01:01:12 +0200

    WIP: database bridge

--- preference_collection/preference_collector.py
+++ preference_collection/preference_collector.py
@@ -1,5 +1,9 @@
 from abc import ABC, abstractmethod
 from uuid import uuid4
+from sys import path
+import os
+import django
+
 
 from preference_collection.preference_oracle import RewardMaximizingOracleMixin
 from query_selection.query_selector import AbstractQuerySelectorMixin, MostRecentlyGeneratedQuerySelectorMixin
@@ -32,6 +36,12 @@ class BaseHumanPreferenceCollectorMixin(AbstractPreferenceCollectorMixin, MostRe
     def __init__(self, preferences, query_candidates, output_path='./videofiles/'):
         super().__init__(preferences, query_candidates)
         self.renderer = SegmentRenderer(output_path=output_path)
+        self._queried_prefs = []
+
+        # preparations for django connection
+        path.append('D:/Sascha/BA/webapp/pbrlwebapp')
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pbrlwebapp.settings')
+        django.setup()
 
     def query_preferences(self, num_preferences):
         queries = self.select_queries(
@@ -39,10 +49,25 @@ class BaseHumanPreferenceCollectorMixin(AbstractPreferenceCollectorMixin, MostRe
         for query in queries:
             uuid = uuid4()
             str_id = str(uuid)
-            self.renderer.render_segment(query[0], '{}-left'.format(str_id))
-            self.renderer.render_segment(query[1], '{}-right'.format(str_id))
-            # add query to db, link to saved clips
+            url_right = self.renderer.render_segment(
+                query[0], '{}-left'.format(str_id))
+            url_left = self.renderer.render_segment(
+                query[1], '{}-right'.format(str_id))
 
-    def collect_preferences():
-        # TODO implement collection
-        pass
+            from preferences import models
+            unlabeled_preference = models.Preference.objects.create(
+                video_url_left=url_left, video_url_right=url_right)
+            unlabeled_preference.save()
+            self._queried_prefs.append(
+                {'id': unlabeled_preference.id, 'query': query, 'label': None})
+            debug = 1+1
+
+    def collect_preferences(self):
+        from preferences import models
+        for pref in self._unlabeled_prefs():
+            db_pref = models.Preference.objects.get(pk=pref.id)
+            
+
+    @property
+    def _unlabeled_prefs(self):
+        return [pref for pref in self._queried_prefs if pref['label'] is not None]
--- video/segment_renderer.py
+++ video/segment_renderer.py
@@ -1,4 +1,4 @@
-import random
+import os
 import numpy as np
 import cv2
 from cv2 import VideoWriter, VideoWriter_fourcc
@@ -8,17 +8,18 @@ class SegmentRenderer:
 
     def __init__(self, output_path):
         self.output_path = output_path
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
 
-    def render_segment(self, segment, name):
+    def render_segment(self, segment, name, fps=12, fourcc=VideoWriter_fourcc(*'DIVX')):
         outfile = '{}{}.avi'.format(self.output_path, name)
-        fps = 12
-        fourcc = VideoWriter_fourcc(*'DIVX')
         singleframe = np.array(segment.frames[0])
         fshape = (singleframe.shape[1], singleframe.shape[0])
 
         vid_writer = VideoWriter(outfile, fourcc, fps, fshape)
-        
+
         for frame in segment.frames:
             vid_writer.write(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
 
         vid_writer.release()
+        return outfile
