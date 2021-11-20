8c6889c5dfe8b389ecb073bf81d82fe73abeb8cd
Robert-Held <schoenfeld@cnd-productions.de>
2021-11-20 19:01:43 +0100

    feat: Adopted develop refactoring to run script

--- run-gridworld.py
+++ run-gridworld.py
@@ -19,9 +19,12 @@ def main():
     # env = create_env("MountainCar-v0", termination_penalty=10.)
     env = create_env(levelname, frame_stack_depth=0,
                      gridworld_settings=gridworld_settings)
-    agent = PbRLAgent(env=env, reward_model_name="Mlp", num_pretraining_epochs=8, 
-                    num_training_epochs_per_iteration=16)
-    agent.pb_learn(num_training_timesteps=200000, num_pretraining_preferences=512)
+    agent = PbRLAgent(env=env, reward_model_name="Mlp",
+                      num_pretraining_epochs=8, 
+                      num_training_iteration_epochs=16)
+    agent.pb_learn(num_training_timesteps=200000,
+                   num_training_preferences=1000,
+                   num_pretraining_preferences=512)
 
     env.close()
 
