616a82d30086e33a5457f4d093111b63c2b0e6ca
sascha <sascha.nebel@student.kit.edu>
2021-12-01 11:01:52 +0100

    Refactored querents and factories to separate files

--- agent_factory/rl_teacher_factory.py
+++ agent_factory/rl_teacher_factory.py
@@ -1,26 +1,33 @@
 from typing import Type
 
-from agent_factory.agent_factory import AbstractAgentFactory
 from agents.policy_model import PolicyModel
 from agents.preference_based.buffered_policy_model import BufferedPolicyModel
 from environment_wrappers.internal.reward_monitor import RewardMonitor
 from environment_wrappers.internal.reward_predictor import RewardPredictor
-from environment_wrappers.internal.reward_standardizer import RewardStandardizer
-from environment_wrappers.internal.trajectory_buffer import TrajectoryBuffer, FrameTrajectoryBuffer
-from preference_collector.human_preference.human_preference_collector import HumanPreferenceCollector
-from preference_collector.preference_collector import AbstractPreferenceCollector
-from preference_collector.synthetic_preference.preference_oracle import RewardMaximizingOracle
-from preference_collector.synthetic_preference.synthetic_preference_collector import SyntheticPreferenceCollector
-from preference_querent.preference_querent import AbstractPreferenceQuerent, HumanPreferenceQuerent
-from preference_querent.query_selector.query_selector import RandomQuerySelector
-from preference_querent.dummy_preference_querent import DummyPreferenceQuerent
+from environment_wrappers.internal.reward_standardizer import \
+    RewardStandardizer
+from environment_wrappers.internal.trajectory_buffer import \
+    FrameTrajectoryBuffer
+from preference_collector.human_preference.human_preference_collector import \
+    HumanPreferenceCollector
+from preference_collector.preference_collector import \
+    AbstractPreferenceCollector
+from preference_querent.human_preference_querent import HumanPreferenceQuerent
+from preference_querent.preference_querent import AbstractPreferenceQuerent
+from preference_querent.query_selector.query_selector import \
+    RandomQuerySelector
 from query_generator.choice_set.choice_set_generator import ChoiceSetGenerator
-from query_generator.choice_set.segment.pretraining_segment_sampler import RandomPretrainingSegmentSampler
-from query_generator.choice_set.segment.segment_sampler import RandomSegmentSampler
+from query_generator.choice_set.segment.pretraining_segment_sampler import \
+    RandomPretrainingSegmentSampler
+from query_generator.choice_set.segment.segment_sampler import \
+    RandomSegmentSampler
 from query_generator.query_generator import AbstractQueryGenerator
 from query_generator.query_item_selector import RandomItemSelector
-from query_schedule.query_schedule import AbstractQuerySchedule, ConstantQuerySchedule
+from query_schedule.query_schedule import (AbstractQuerySchedule,
+                                           ConstantQuerySchedule)
 from reward_model_trainer.reward_model_trainer import RewardModelTrainer
+
+from agent_factory.agent_factory import AbstractAgentFactory
 
 
 class RLTeacherFactory(AbstractAgentFactory):
@@ -57,23 +64,3 @@ class RLTeacherFactory(AbstractAgentFactory):
 
     def create_query_schedule_cls(self) -> Type[AbstractQuerySchedule]:
         return ConstantQuerySchedule
-
-
-class SyntheticRLTeacherFactory(RLTeacherFactory):
-
-    def __init__(self, segment_length=25):
-        super().__init__(segment_length=segment_length)
-
-    def create_env(self, env, reward_model):
-        env = TrajectoryBuffer(env)
-        env = RewardPredictor(env, reward_model)
-        env = RewardStandardizer(env)
-        env = RewardMonitor(env)
-        return env
-
-    def create_preference_collector(self) -> AbstractPreferenceCollector:
-        self.preference_collector = SyntheticPreferenceCollector(oracle=RewardMaximizingOracle())
-        return self.preference_collector
-
-    def create_preference_querent(self) -> AbstractPreferenceQuerent:
-        return DummyPreferenceQuerent(query_selector=RandomQuerySelector())
