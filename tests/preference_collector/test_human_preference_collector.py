155498091edcdf8afc8f9acaa75545a2faba2cba
Sascha Nebel <sascha.nebel@student.kit.edu>
2021-12-07 23:33:53 +0100

    Improved test

--- tests/preference_collector/test_human_preference_collector.py
+++ tests/preference_collector/test_human_preference_collector.py
@@ -2,14 +2,17 @@ import pytest
 import django
 import os
 import sys
+from preference_collector.binary_choice import BinaryChoice
 from preference_collector.human_preference.human_preference_collector import HumanPreferenceCollector
 from query_generator.query import Query
 
 
 def test_human_pref_collector(preference_collector):
-    just_collected_preferences =preference_collector.collect_preferences()
-    assert len(just_collected_preferences)>0
-    
+    just_collected_preferences = preference_collector.collect_preferences()
+    assert len(just_collected_preferences) == 3
+    assert just_collected_preferences[0].choice == BinaryChoice.LEFT
+    assert just_collected_preferences[1].choice == BinaryChoice.INDIFFERENT
+    assert just_collected_preferences[2].choice == BinaryChoice.RIGHT
 
 
 @pytest.fixture()
@@ -29,12 +32,12 @@ def preference_collector():
         pref.full_clean()
         pref.save()
         i -= .5
-    
+
     preference_collector = HumanPreferenceCollector()
     preference_collector.pending_queries = pending_queries
 
     yield preference_collector
 
-    prefs_to_delete=models.Preference.objects.order_by('-id')[:3]
+    prefs_to_delete = models.Preference.objects.order_by('-id')[:4]
     for pref in prefs_to_delete:
         pref.delete()
