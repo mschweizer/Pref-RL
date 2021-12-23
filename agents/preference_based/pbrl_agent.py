10ab6184fb283b7603c1562d60073ed7d09175c9
marvin.schweizer <marvin.schweizer@kit.edu>
2021-12-23 17:07:38 +0100

    Add type hints to agent classes

--- agents/preference_based/pbrl_agent.py
+++ agents/preference_based/pbrl_agent.py
@@ -4,30 +4,34 @@ from typing import Union
 
 from agents.preference_based.pbrl_callback import PbRLCallback
 from agents.rl_agent import RLAgent
+from preference_collector.preference_collector import AbstractPreferenceCollector
+from preference_querent.preference_querent import AbstractPreferenceQuerent
+from query_generator.query_generator import AbstractQueryGenerator
 from query_schedule.query_schedule import AbstractQuerySchedule
+from reward_model_trainer.reward_model_trainer import RewardModelTrainer
+from reward_models.base import BaseModel
 
 
 class PbRLAgent(RLAgent):
-    # TODO: add type hints to injected components
     def __init__(self, policy_model, pretraining_query_generator, query_generator, preference_querent,
                  preference_collector, reward_model_trainer, reward_model, query_schedule_cls,
                  pb_step_freq, num_epochs_in_pretraining=10, num_epochs_in_training=10):
 
         super(PbRLAgent, self).__init__(policy_model)
 
-        self.query_schedule_cls = query_schedule_cls
+        self.query_schedule_cls: type[AbstractQuerySchedule] = query_schedule_cls
         self.query_schedule: Union[AbstractQuerySchedule, None] = None
 
-        self.pretraining_query_generator = pretraining_query_generator
-        self.query_generator = query_generator
-        self.preference_querent = preference_querent
-        self.preference_collector = preference_collector
-        self.reward_model = reward_model
-        self.reward_model_trainer = reward_model_trainer
+        self.pretraining_query_generator: AbstractQueryGenerator = pretraining_query_generator
+        self.query_generator: AbstractQueryGenerator = query_generator
+        self.preference_querent: AbstractPreferenceQuerent = preference_querent
+        self.preference_collector: AbstractPreferenceCollector = preference_collector
+        self.reward_model: BaseModel = reward_model
+        self.reward_model_trainer: RewardModelTrainer = reward_model_trainer
 
-        self.pb_step_freq = pb_step_freq
-        self.num_epochs_in_pretraining = num_epochs_in_pretraining
-        self.num_epochs_in_training = num_epochs_in_training
+        self.pb_step_freq: int = pb_step_freq
+        self.num_epochs_in_pretraining: int = num_epochs_in_pretraining
+        self.num_epochs_in_training: int = num_epochs_in_training
 
     def predict_reward(self, observation):
         return self.reward_model(observation)
