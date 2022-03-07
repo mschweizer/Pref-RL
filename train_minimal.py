c6b845c7558a6b94551a759f1ff79d3b1b3dddc3
Robert-Held <schoenfeld@cnd-productions.de>
2022-03-07 16:42:35 +0100

    wip: Training the agent

--- train_minimal.py
+++ train_minimal.py
@@ -2,23 +2,25 @@ import argparse
 import logging
 import numpy as np
 import time
+from stable_baselines3 import A2C
 
 # from agent_factory.rl_teacher_factory import SyntheticRLTeacherFactory
-from agent_factory.risk_sensitive_rl_teacher_factory import \
-    RiskSensitiveRLTeacherFactory
+# from agent_factory.risk_sensitive_rl_teacher_factory import \
+#     RiskSensitiveRLTeacherFactory
 # from environment_wrappers.utils import create_env
-from preference_collector.synthetic_preference.preference_oracle import (
-    ProspectTheoryParams,
-    ProspectTheoryUtility
-)
-from environment_wrappers.utils import (
-    add_external_env_wrappers,
-    create_env
-)
+# from preference_collector.synthetic_preference.preference_oracle import (
+#     ProspectTheoryParams,
+#     ProspectTheoryUtility
+# )
+from environment_wrappers.utils import create_env
 
 from gym_gridworld import create_gridworld_env
 
-from gym.wrappers import TransformObservation
+from gym.wrappers import (
+    GrayScaleObservation,
+    FrameStack,
+    TransformObservation
+)
 
 
 def create_cli():
@@ -41,7 +43,7 @@ def create_cli():
     parser.add_argument('--episode_length', default=50, type=int)
     parser.add_argument('--discount_factor', default=1., type=float)
     parser.add_argument('--slip_probab', default=.1, type=float)
-    parser.add_argument('--absorbing_states', default=1, type=int)
+    parser.add_argument('--absorbing_states', default=0, type=int)
     parser.add_argument('--num_posttraining_episodes', default=100, type=int)
     parser.add_argument('--verbose', default=0, type=int)
     return parser
@@ -52,24 +54,24 @@ def main():
     args = cli.parse_args()
 
     print('\n\n==================== Executing Program ====================\n')
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
 
     print('------ 1. Parsing Arguments, Configuring Pref-RL ----------\n')
     # Parameters taken from Ratliff & Mazumdar (2020)
-    risk_sensitive_profiles = [
-        # standard risk-neutral
-        ProspectTheoryParams(coefficient_gain=1, exponent_gain=.9,
-                             coefficient_loss=1, exponent_loss=1.1),
-        # risk-seeking
-        ProspectTheoryParams(coefficient_gain=1, exponent_gain=1.5,
-                             coefficient_loss=.1, exponent_loss=.5),
-        # risk-averse
-        ProspectTheoryParams(coefficient_gain=1, exponent_gain=.8,
-                             coefficient_loss=5, exponent_loss=1.1),
-        # true risk-neutral
-        ProspectTheoryParams(coefficient_gain=1, exponent_gain=1,
-                             coefficient_loss=1, exponent_loss=1)
-    ]
+    # risk_sensitive_profiles = [
+    #     # standard risk-neutral
+    #     ProspectTheoryParams(coefficient_gain=1, exponent_gain=.9,
+    #                          coefficient_loss=1, exponent_loss=1.1),
+    #     # risk-seeking
+    #     ProspectTheoryParams(coefficient_gain=1, exponent_gain=1.5,
+    #                          coefficient_loss=.1, exponent_loss=.5),
+    #     # risk-averse
+    #     ProspectTheoryParams(coefficient_gain=1, exponent_gain=.8,
+    #                          coefficient_loss=5, exponent_loss=1.1),
+    #     # true risk-neutral
+    #     ProspectTheoryParams(coefficient_gain=1, exponent_gain=1,
+    #                          coefficient_loss=1, exponent_loss=1)
+    # ]
 
     env_id: str = args.env_id
     reward_model: str = args.reward_model
@@ -94,11 +96,6 @@ def main():
     obs_to_grayscale_wrapper = True if args.obs_to_grayscale == 1 \
         else False
 
-    assert risk_sensitive_profiles[args.risk_attitude] is not None, \
-        f'No risk attitude stored under index {args.risk_attitude}.'
-    utility_model = ProspectTheoryUtility(
-        risk_sensitive_profiles[args.risk_attitude])
-
     absorbing_states: bool
     assert args.absorbing_states in [0, 1],  \
         f"Values 0 and 1 allowed for argument '--absorbing_states', " \
@@ -122,21 +119,9 @@ def main():
         "given."
     verbose = True if args.verbose == 1 else False
 
-    # print('------ 2. CartPole Env Creation --------------------------\n')
-    # env_id = "CartPole-v1"
-    # env = create_env(env_id, termination_penalty=10.)
-
-    # print('------ 2. Pong-v0 Env Creation ----------------------------\n')
-    # env_id = "Pong-v0"
-    # env = create_env(env_id, termination_penalty=10.)
-
-    # env = add_external_env_wrappers(env, termination_penalty,
-    #                                 frame_stack_depth,
-    #                                 obs_to_grayscale_wrapper)
-
     print('------ 2. Environment Creation ----------------------------\n')
     if env_id.startswith('Lab2D-'):
-        gridworld = create_gridworld_env(
+        env = create_gridworld_env(
             env_id=env_id,
             env_settings={
                 'max_episode_steps': episode_length,
@@ -150,10 +135,10 @@ def main():
                 },
             }
         )
-        env = add_external_env_wrappers(gridworld, termination_penalty,
-                                        frame_stack_depth,
-                                        obs_to_grayscale_wrapper)
-
+        if obs_to_grayscale_wrapper:
+            env = GrayScaleObservation(env)
+        if frame_stack_depth:
+            env = FrameStack(env, num_stack=frame_stack_depth)
         # Add dummy dimension via wrapper
         if frame_stack_depth and obs_to_grayscale_wrapper:
             env = TransformObservation(env, lambda obs: np.array([obs]))
@@ -161,40 +146,22 @@ def main():
         env = create_env(args.env_id, termination_penalty=termination_penalty,
                          frame_stack_depth=frame_stack_depth)
 
-    print('------ 3. Creating Agent ----------------------------------\n')
-    # factory = SyntheticRLTeacherFactory(
-    factory = RiskSensitiveRLTeacherFactory(
-        policy_train_freq=policy_train_freq,
-        pb_step_freq=pb_step_freq,
-        reward_training_freq=reward_training_freq,
-        num_epochs_in_pretraining=pretraining_epochs,
-        num_epochs_in_training=training_epochs,
-        utility_model=utility_model,
-        segment_length=segment_length)
-    agent = factory.create_agent(env=env, reward_model_name=reward_model)
+    model = A2C('MlpPolicy', env, verbose=verbose)
+    model.learn(total_timesteps=num_rl_timesteps)
 
-    print('------ 4. Starting PbRL Run -------------------------------\n')
-    agent.pb_learn(num_training_timesteps=num_rl_timesteps,
-                   num_training_preferences=num_training_preferences,
-                   num_pretraining_preferences=num_pretraining_preferences)
-
-    print('------ 5. Training Finished. Observing Results ------------\n')
     obs = env.reset()
     for ep in range(posttraining_episodes):
         done = False
         while not done:
-            action, _state = agent.choose_action(obs)
-            logging.getLogger().debug(f'chosen action: {action}')
-            _, _, _, info = env.step(action)
+            action, _state = model.predict(obs)
+            # logging.getLogger().info(f'chosen action: {action}')
+            obs, reward, done, info = env.step(action)
+            # logging.getLogger().info(f'step reward: {reward}')
             env.render(mode='WORLD.RGB')
-            done = info['original_done']
             time.sleep(0.1)
         obs = env.reset()
         print(f'-- Episode {ep+1} finished, environment reset.')
         time.sleep(2)
-
-    print('------ 6. Run Finished. Cleaning Up -----------------------\n')
-    env.close()
 
 
 if __name__ == '__main__':
