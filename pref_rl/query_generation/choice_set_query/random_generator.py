b7ccf221924b89b5c1b41f35c32e453d6ec4a7c6
marvin.schweizer <marvin.schweizer@kit.edu>
2022-08-12 09:48:31 +0200

    Add logger

--- pref_rl/query_generation/choice_set_query/random_generator.py
+++ pref_rl/query_generation/choice_set_query/random_generator.py
@@ -1,20 +1,23 @@
-import logging
 import random
 from typing import List, Tuple
 
-from pref_rl.query_generation.choice_set_query.generator import AbstractChoiceSetQueryGenerator
+from .generator import AbstractChoiceSetQueryGenerator
+from ...utils.logging import create_logger
 
 
 class RandomChoiceSetQueryGenerator(AbstractChoiceSetQueryGenerator):
+    def __init__(self, alternative_generator, alternatives_per_choice_set=2):
+        super().__init__(alternative_generator, alternatives_per_choice_set)
+        self.logger = create_logger("RandomChoiceSetQueryGenerator")
+
     def select_choice_sets(self, num_choice_sets: int, num_alternatives_per_choice_set: int, alternatives) \
             -> List[Tuple]:
         return [self._select_alternatives(alternatives, num_alternatives_per_choice_set)
                 for _ in range(num_choice_sets)]
 
-    @staticmethod
-    def _select_alternatives(alternatives, num_alternatives) -> Tuple:
+    def _select_alternatives(self, alternatives, num_alternatives_per_choice_set) -> Tuple:
         try:
-            return tuple(random.sample(alternatives, num_alternatives))
+            return tuple(random.sample(alternatives, num_alternatives_per_choice_set))
         except ValueError as e:
-            logging.warning(str(e) + " Returning empty sample.")
+            self.logger.warning(str(e) + " Returning empty sample.")
             return tuple()
