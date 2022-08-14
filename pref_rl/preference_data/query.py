cce50a577d3a723e6fce5f5c1fab5bff7f6115f8
marvin.schweizer <marvin.schweizer@kit.edu>
2022-08-14 20:58:39 +0200

    Add type annotations and docstrings

--- pref_rl/preference_data/query.py
+++ pref_rl/preference_data/query.py
@@ -1,13 +1,22 @@
 import uuid
+from typing import List
 
 
 class Query:
     def __init__(self):
+        """
+        A (preference) query is sent to a user to elicit their preferences.
+        """
         self.id = str(uuid.uuid4())  # TODO: Add env and experiment prefix
 
 
 class ChoiceSetQuery(Query):
-    def __init__(self, choice_set):
+    def __init__(self, choice_set: List):
+        """
+        A choice set query is a special type of preference query that asks the user to make a choice among a (finite)
+        set of alternatives.
+        :param choice_set: The set of alternatives the user chooses from.
+        """
         assert len(choice_set) > 1, "A choice set must have at least two elements."
         super().__init__()
         self.choice_set = choice_set
@@ -15,11 +24,15 @@ class ChoiceSetQuery(Query):
     def __len__(self):
         return len(self.choice_set)
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: int):
         return self.choice_set[key]
 
 
 class BinaryChoiceSetQuery(ChoiceSetQuery):
-    def __init__(self, choice_set):
+    def __init__(self, choice_set: List):
+        """
+        A binary choice set query has exactly two alternatives in the choice set.
+        :param choice_set: The set of alternatives the user chooses from.
+        """
         assert len(choice_set) == 2, "The choice set of a binary choice must have exactly two alternatives."
         super(BinaryChoiceSetQuery, self).__init__(choice_set)
